29ca3ddecc2de7659dad14bfa2f9bcd492963470
CodieMisiani <nimrodmisiani1@gmail.com>
2025-07-15 17:13:52 +0300

    feat: add variable examples and applied them to expense tracker

---
+++ basics/variables.py
@@ -0,0 +1,12 @@
+#Write code that:
+
+#Stores the name of your best friend
+
+#Stores how many shillings they spent on lunch
+
+#Prints: “Alex spent 400 KES on lunch.” (Use print() and variables)
+
+user_best_friend = "Misiani"
+lunch_spending = 1000
+currency = "KES"
+print(f"{user_best_friend}, spent {lunch_spending} {currency} on lunch")
---
+++ solo_project/main.py
@@ -0,0 +1,14 @@
+# personal details
+user_name = "Nimrod"
+monthly_budget = 50000  # in KES
+is_tracking_enabled = True
+currency = "KES"
+
+# print intro message
+print("Welcome,", user_name)
+print("Your budget this month is:", monthly_budget)
+print("Your budget this month is:", monthly_budget,f"You're using {currency} as your currency.")
+print("Your budget this month is:", monthly_budget,f" {currency}.")
+print("Tracking expenses:", is_tracking_enabled)
+print(f"You're using {currency} as your currency.")
+
