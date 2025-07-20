4f2a4a3b9cddafed976a1ab56f06da13b61d812b
CodieMisiani <nimrodmisiani1@gmail.com>
2025-07-20 23:22:39 +0300

    feat: add user bestfriend four

--- basics/variables.py
+++ basics/variables.py
@@ -10,6 +10,7 @@ user_best_friend = "Misiani"
 user_best_friend2 = "Moranga"
 user_best_friend3 = "Lincoln"
 user_best_friend4 = "Adrian"
+user_best_friend4 = "Nimrod"
 lunch_spending = 1000
 currency = "KES"
 print(f"{user_best_friend}, spent {lunch_spending} {currency} on lunch")
